b428e310f26d62cb2cb141b5f5abf44912050e13
Ian Whittall <whittallian@gmail.com>
2019-04-04 05:53:46 +0000

    ROT Cipher; READS text from stdin; Reads key from stdin; Some comments. Cant remember why some stuff is there but code doesnt work other wise. Works in online GDB. Need to learn to use stdin in CHE

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h>  //Includes standard library
+#include <string.h>  //Includes String Library
 
 
 
 
 int main() {
     
-    const char str1[100] = "THIS IS TEXT";
-    char str2[100];
+    
+    char str2[10000];  //Initialising str2 to handle up to 10000 characters
     
     
-    int i=0, k, r=0;
+    int i=0, k;  //Initialising variables i and k as integers. i is used as a counter in a loop later, and k is used as the encryption key
     
-    printf("\nEnter Eccryption Text: ");    
-    scanf("%[^\n]", str2);
+    printf("\nEnter Eccryption Text: ");    //tells user to enter text to encrypt
+    scanf("%[^\n]", str2);   //reads text user inputs and stores as string str2
+    /*
+     So I think this just reads through and scans until
+     it reaches a new line, rather than whitespace as %s
+     normally does.
+     */
     
     
     
     
-    start:printf("\nEnter Encryption Key:");
-    scanf("%d", &k);
+    start:printf("\nEnter Encryption Key:"); // tells user to enter encryption key
+    scanf("%d", &k);  // scans and stores encryprion key as variable k
     
     if(k<0 || k>26){
         printf("Key must be between 0 and 26\n");
@@ -28,6 +33,14 @@ int main() {
     else if(k>=0 && k<=26){
         
     }
+    /*
+     The If - Else if loop detects if k is an integer between 0 and 26
+     If K is outside these values it tells the user it must be between 0 and 26
+     and jumps back to telling the user to enter a key, then scans what the user imputs
+     and does this until its a number between 0 and 26
+     But if you enter a letter it runs and doesnt cipher the text
+     */
+    
     
     for(i=0; i<=strlen(str2); i++){
         
