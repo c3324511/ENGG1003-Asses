ab7748b621486b679741ea564d78c9cfa3bf1741
Ian Whittall <whittallian@gmail.com>
2019-04-04 04:31:23 +0000

    ROT13 Cipher encryption; Hard Coded Input; Hard Coded Key

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 
 int main() {
     
-    const char str1[100] = "This is Text";
+    const char str1[100] = "THIS IS TEXT";
     char str2[100];
     strcpy(str2, str1);
     
-    printf("%s", str1);
+    int i=0, k=13;
+    
+    for(i=0; i<=strlen(str2); i++){
+        
+        if(str2[i] == 32){
+            continue;
+        }
+        else if(str2[i]>=65 && str2[i]<=90){
+        str2[i] = str2[i]-65;
+        str2[i] = ((str2[i] + k)%26);
+        str2[i] = str2[i]+65;
+        }
+
+    }
+    
+    
+    printf("%s", str2);
     
     
     
