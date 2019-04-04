320b8ac72dc26deb83f99629729b45b7aab24d0c
Ian Whittall <whittallian@gmail.com>
2019-04-04 04:10:39 +0000

    Playing around with strings

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
+
+
 
 int main() {
+    
+    const char str1[100] = "This is Text";
+    char str2[100];
+    strcpy(str2, str1);
+    
+    printf("%s", str1);
     
     
     
