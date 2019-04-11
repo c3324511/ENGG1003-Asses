f073f7ad63a3968fec9f311162fb36c7e20e5d21
Ian Whittall <whittallian@gmail.com>
2019-04-11 06:11:20 +0000

    All working pretty good. Now for decryption cipher.

--- SubEncrypt/SubEncrypt.h
+++ SubEncrypt/SubEncrypt.h
@@ -1,2 +1,6 @@
-char SubEncrypt[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char EncryptKey[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+char SubEncrypt[] = "adshfoi;sdf$%*^&(*()   123"; //Enter Text to Encrypt
+
+
+char EncryptKey[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"; 
+/*Enter Encryption Key, with first character what 'a' should equal, and second character what 'b' should equal, etc.
+ through alphabet, with 27th what 'A' should equal, 28th what 'B' should equal, etc.*/
