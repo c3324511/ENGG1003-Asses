f2b2469e05882cf37bf59524f77fae9a94456309
Ian Whittall <whittallian@gmail.com>
2019-04-12 12:43:51 +0000

    SubEncrypt2; Implements sub encryption, but with a lot less code then SubEncrypt1

---
+++ SubEncrypt/SubEncrypt2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "SubEncrypt.h"
+
+int main(){
+
+    for(int i = 0; i<=strlen(SubEncrypt); i++){
+    
+        if(SubEncrypt[i]>='a' && SubEncrypt[i]<='z'){
+        SubEncrypt[i] = EncryptKey[ ((int) SubEncrypt[i]) - ((int) 'a')];
+        }
+        else if(SubEncrypt[i]>='A' && SubEncrypt[i]<='Z'){
+        SubEncrypt[i] = EncryptKey[ ((int) SubEncrypt[i]) - ((int) 'A')];
+        }
+        
+
+    }
+    
+    
+    printf("%s", SubEncrypt);  
+
+
+
+
+}
