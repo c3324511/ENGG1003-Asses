f89346d1ea7bfaa2bc1ac48443b1775fe404ef88
Ian Whittall <whittallian@gmail.com>
2019-04-26 04:59:15 +0000

    -

--- SubDecrypt-K.c
+++ SubDecrypt-K.c
@@ -9,7 +9,7 @@ int main(){
     for(int i=0; i<=strlen(SDecryptKey); i++){
         for(int j=0; j<=strlen(SubDecrypt); j++){
             if(SDecryptKey[i] == SubDecrypt[j]){
-                SubDecrypt[j] = i+1000; // To Put it outside ascii range so it doesnt get confused with other characters
+                SubDecrypt[j] = i+1000+65; // To Put it outside ascii range so it doesnt get confused with other characters
             }
         }
         
