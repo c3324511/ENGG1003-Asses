e6cb955137254af8f7b86249f2cf5a9d9cdcc4de
Ian Whittall <whittallian@gmail.com>
2019-04-27 08:57:20 +0000

    Made SubDecrypt-K.c wor because I AM A MOTHERFUCKING GENIUS

--- SubDecrypt-K.c
+++ SubDecrypt-K.c
@@ -6,24 +6,26 @@
 
 int main(){
     
-    char SubDecryptCopy[strlen(SubDecrypt)];
     
-   for(int i=0; i<=strlen(SDecryptKey); i++){
-        for(int j=0; j<=strlen(SubDecrypt); j++){
-            if(SDecryptKey[i] == SubDecrypt[j] && SubDecrypt[j] != ' ' ){
+    char SubDecryptCopy[10000];
+    
+    for(int i=0; i<strlen(SDecryptKey); i++){
+        for(int j=0; j<strlen(SubDecrypt); j++){
+            if(SDecryptKey[i] == SubDecrypt[j] ){
                 SubDecryptCopy[j] = i+1000+65; // To Put it outside ascii range so it doesnt get confused with other characters
             }
         }
     }
     
-    for(int l=0; l<=strlen(SubDecrypt); l++){
+    for(int l=0; l<strlen(SubDecrypt); l++){
         if(SubDecrypt[l]>='A' && SubDecrypt[l]<='Z'){
             SubDecryptCopy[l] = SubDecryptCopy[l] - 1000;
         }
+        else if(SubDecrypt[l] == 32){
+            SubDecryptCopy[l] = 32;
+        }
         
     }
-    
-    
     
     printf("%s", SubDecryptCopy);
     
