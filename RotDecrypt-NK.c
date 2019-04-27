163ce769bfc65f609245b9abfad7ffab91cd6547
Ian Whittall <whittallian@gmail.com>
2019-04-27 06:53:16 +0000

    RotDecryptNK works but still needs a person to pick out which one is the correct one. Something weird happens with the compiler? shouldnt be an error with the code.

--- RotDecrypt-NK.c
+++ RotDecrypt-NK.c
@@ -4,21 +4,28 @@
 
 int main(){
     
-    char RotDecryptNK[] = "uryyb jbeyq";
-    printf("%s", RotDecryptNK);
-    int k=13;    
-     
-    for(int i=0; i<=100; i++){
+    char RotDecryptNK[] = "GDKKN VNQKC";
+    
         
-        if(RotDecryptNK[i]>=65 && RotDecryptNK[i]<=90){
-            RotDecryptNK[i] = RotDecryptNK[i]-65 +26 ;  //Moves ASCII values to between 0 and 26 by substracting ASCII value for A
-            RotDecryptNK[i] = ((RotDecryptNK[i] - k)%26); //Rotates 13 places around alphabet. The % ensures it works for letters later by skipping back to start of alphabet
-            RotDecryptNK[i] = RotDecryptNK[i]+65; //Moves values back to ASCII values.
-            printf("%s", RotDecryptNK);
-     }   
+    int i=0;
+    
+    for(int k=1; k<=25; k++){
+        
+        for(i=0; i<=1000; i++){
+        
+            if(RotDecryptNK[i]>=64 && RotDecryptNK[i]<=90){
+                RotDecryptNK[i] = RotDecryptNK[i]-65+26;
+                RotDecryptNK[i] = ((RotDecryptNK[i] - k)%26);
+                RotDecryptNK[i] = RotDecryptNK[i]+65; 
+                
+            }   
 
-    }    
+        }   
+        
+        printf("\n%d: %s", k, RotDecryptNK);
+    }
 
-    printf("%s", RotDecryptNK);
+
+    
     
 }
