b668aeef65bbe6c98f98ae18a9835562fded7e34
Ian Whittall <whittallian@gmail.com>
2019-04-27 08:57:47 +0000

    just everything

---
+++ RotDecrypt-NK/RotDecrypt-NK.h
@@ -0,0 +1,1 @@
+char RotDecryptNK[] = "uryyb jbeyq";
--- SubEncrypt/SubEncrypt.h
+++ SubEncrypt/SubEncrypt.h
@@ -1,6 +1,6 @@
 char SubEncrypt[] = "adshfoi;sdf$%*^&(*()   123"; //Enter Text to Encrypt
 
 
-char EncryptKey[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"; 
+char EncryptKey[] = "QWERTYUIOPASDFGHJKLZXCVBNMQWERTYUIOPASDFGHJKLZXCVBNM"; 
 /*Enter Encryption Key, with first character what 'a' should equal, and second character what 'b' should equal, etc.
  through alphabet, with 27th what 'A' should equal, 28th what 'B' should equal, etc.*/
